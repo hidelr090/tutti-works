bf3f1dfada8bb838b33c6ff74c4c04890431f227
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-04-20 16:18:37 -0300

    test: ensure that encrypt method returns a token

--- tests/infra/cryptography/jwt-adapter.spec.ts
+++ tests/infra/cryptography/jwt-adapter.spec.ts
@@ -28,6 +28,14 @@ describe('JwtAdapter', () =>{
       expect(signSpy).toHaveBeenCalledWith({id: 'any_id'}, 'secret');
     });
 
+    test('Should return a token on sign success', async () =>{
+      const sut = makeSut();
+      
+      const accessToken = await sut.encrypt('any_id');
 
+      expect(accessToken).toBe('any_token');
+    });
+
+    
   });
 })
