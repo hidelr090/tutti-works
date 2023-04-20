ad55f10118bed533da01827ceb416430efaef2ff
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-04-20 16:47:12 -0300

    feat: add email validator adapter

---
+++ src/infra/validators/email-validator-adapter.ts
@@ -0,0 +1,11 @@
+import { EmailValidator } from "@/validation/validators";
+
+import validator from 'validator';
+
+export class EmailValidatorAdapter implements EmailValidator {
+
+  isValid (email: string): boolean {
+    return validator.isEmail(email);
+  }
+
+}
