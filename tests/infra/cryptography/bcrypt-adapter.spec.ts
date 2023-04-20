da8fa5dd218fd2d02d9ad345c30ced5c69c72b08
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-04-20 14:07:37 -0300

    test: ensure that compare method returns true when comparison is successful

--- tests/infra/cryptography/bcrypt-adapter.spec.ts
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -60,6 +60,13 @@ describe ('BcryptAdapter', () => {
 
       expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash');
     });
+  
+    test('Should return true when comparison is successful', async ()=> {
+      const sut = makeSut();
+      
+      const isValid = await sut.compare('any_value', 'any_hash');
 
+      expect(isValid).toBeTruthy();
+    });
   });
 })
