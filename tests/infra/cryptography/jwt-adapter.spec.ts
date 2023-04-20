61bbb9417ec611452b397a195fa25b6f19dea356
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-04-20 16:14:57 -0300

    test: ensure that sign method is called with correct values

---
+++ tests/infra/cryptography/jwt-adapter.spec.ts
@@ -0,0 +1,33 @@
+import { JwtAdapter } from '@/infra/cryptography';
+
+import jwt from 'jsonwebtoken';
+
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return 'any_token';
+  },
+
+  async verify(): Promise<string> {
+    return 'any_value';
+  }
+}));
+
+const makeSut = (): JwtAdapter =>{
+  return new JwtAdapter('secret');
+}
+
+describe('JwtAdapter', () =>{
+  describe('sign()', () =>{
+    test('Should call sign with correct values', async () =>{
+      const sut = makeSut();
+      
+      const signSpy = jest.spyOn(jwt, 'sign');
+
+      await sut.encrypt('any_id');
+
+      expect(signSpy).toHaveBeenCalledWith({id: 'any_id'}, 'secret');
+    });
+
+
+  });
+})
