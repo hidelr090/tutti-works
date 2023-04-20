a06a24469b983de8f83388e4143de35d38a6eb35
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-04-20 15:56:55 -0300

    feat: add jwt adapter

---
+++ src/data/protocols/cryptography/decrypter.ts
@@ -0,0 +1,3 @@
+export interface Decrypter {
+  decrypt: (cyphertext: string) => Promise<string>;
+}
---
+++ src/infra/cryptography/jwt-adapter.ts
@@ -0,0 +1,19 @@
+import { Encrypter, Decrypter } from "@/data/protocols/cryptography";
+
+import jwt from 'jsonwebtoken';
+
+export class JwtAdapter implements Encrypter, Decrypter {
+
+  constructor(
+    private readonly secretKey: string
+  ){}
+
+  async encrypt(plaintext: string): Promise<string> {
+    return jwt.sign({id: plaintext}, this.secretKey)
+  }
+
+  async decrypt (cyphertext: string): Promise<string> {
+    return jwt.verify(cyphertext, this.secretKey) as any
+  }
+
+}
