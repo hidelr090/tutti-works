c35dfdd1025bb923bf50977e20bb82a9f62ad2ef
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-04-21 10:48:58 -0300

    feat: add cryptography mock spies

---
+++ tests/data/mocks/mock-cryptography.ts
@@ -0,0 +1,51 @@
+import { Hasher, HashComparer, Encrypter, Decrypter } from "@/data/protocols";
+
+import Chance from "chance";
+
+const chance = new Chance();
+
+export class HasherSpy implements Hasher {
+  plaintext = '';
+  
+  async hash(plaintext: string): Promise<string> {
+    this.plaintext = plaintext;
+    return 'hashed_password';
+  }
+}
+
+export class HashComparerSpy implements HashComparer {
+  plaintext: string | undefined;
+  digest: string | undefined;
+  isValid = true;
+
+  async compare (plaintext: string, digest: string): Promise<boolean>{
+    this.plaintext = plaintext;
+    this.digest = digest;
+    return this.isValid;
+  }
+
+}
+
+export class EncrypterSpy implements Encrypter {
+  cyphertext = chance.guid();
+  plaintext: string | undefined;
+
+  async encrypt (plaintext: string): Promise<string>{
+    this.plaintext = plaintext;
+    return this.cyphertext
+  }
+
+}
+
+export class DecrypterSpy implements Decrypter {
+  plaintext = chance.string({
+    length: 8,
+    pool: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  });
+  cyphertext: string | undefined;
+
+  async decrypt (cyphertext: string): Promise<string> {
+    this.cyphertext = cyphertext;
+    return this.plaintext;
+  }
+}
