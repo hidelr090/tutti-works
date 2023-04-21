9b95ebf29f7bd0326b0b01985df89b7986448b53
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-04-21 10:50:52 -0300

    test: ensure that hasher is called with correct values

---
+++ tests/data/usecases/db-add-user.spec.ts
@@ -0,0 +1,37 @@
+import { DbAddUser } from "@/data/usecases";
+import { mockAddUserParams, throwError } from "@/tests/domain/mocks";
+import { HasherSpy, AddUserRepositorySpy, CheckUserByIdentifierCodeRepositorySpy } from '@/tests/data/mocks';
+
+type SutTypes = {
+  sut: DbAddUser;
+  hasherSpy: HasherSpy;
+  addUserRepositorySpy: AddUserRepositorySpy;
+  checkUserByIdentifierCodeRepositorySpy: CheckUserByIdentifierCodeRepositorySpy;
+}
+
+const makeSut = (): SutTypes => {
+  const checkUserByIdentifierCodeRepositorySpy = new CheckUserByIdentifierCodeRepositorySpy();
+  const hasherSpy = new HasherSpy();
+  const addUserRepositorySpy = new AddUserRepositorySpy();
+  const sut = new DbAddUser(hasherSpy, addUserRepositorySpy, checkUserByIdentifierCodeRepositorySpy);
+  return {
+    sut,
+    hasherSpy,
+    addUserRepositorySpy,
+    checkUserByIdentifierCodeRepositorySpy
+  }
+};
+
+describe('DbAddUser Usecase', () =>{
+  test('Should call Hasher with correct plaintext', async () =>{
+    const { sut, hasherSpy } = makeSut();
+  
+    const addUserParams = mockAddUserParams();
+
+    await sut.add(addUserParams);
+  
+    expect(hasherSpy.plaintext).toBe(addUserParams.password);
+  });
+
+});
+
