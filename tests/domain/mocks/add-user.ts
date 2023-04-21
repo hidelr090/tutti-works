ee054a79880c2a63a64783d3e73877d25030cbb7
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-04-21 10:50:10 -0300

    feat: create mocking for add user params

---
+++ tests/domain/mocks/add-user.ts
@@ -0,0 +1,17 @@
+import { AddUser } from "@/domain/usecases";
+
+import Chance from "chance";
+
+const chance = new Chance();
+
+export const mockAddUserParams = (): AddUser.Params => ({
+  name: chance.name(),
+  email: chance.email(),
+  password: 'valid_password',
+  identifierCode: chance.integer({min: 11, max: 11}).toString(),
+  phone: chance.phone()
+});
+
+//export const mockAuthenticationParams = (): Authentication.Params => ({});
+
+
