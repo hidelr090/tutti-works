2aaf2e1988e64e941cd55190c34e4696a01a221e
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-04-28 17:24:58 -0300

    feat: create mocks for UpdateJobVacancyRepository

--- src/data/usecases/index.ts
+++ src/data/usecases/index.ts
@@ -1,3 +1,4 @@
 export * from './db-add-user';
 export * from './db-add-job-vacancy';
 export * from './db-update-user';
+export * from './db-update-job-vacancy';
--- tests/data/mocks/mock-db-job-vacancy.ts
+++ tests/data/mocks/mock-db-job-vacancy.ts
@@ -1,4 +1,7 @@
-import { AddJobVacancyRepository } from "@/data/protocols/db/repositories";
+import { AddJobVacancyRepository, LoadJobVacancyByIdRepository, UpdateJobVacancyRepository } from "@/data/protocols/db/repositories";
+import { UpdateJobVacancy } from "@/domain/usecases";
+
+import Chance from 'chance';
 
 export class AddJobVacancyRepositorySpy implements AddJobVacancyRepository {
   public params: AddJobVacancyRepository.Params | undefined;
@@ -8,4 +11,38 @@ export class AddJobVacancyRepositorySpy implements AddJobVacancyRepository {
     this.params = data;
     return this.result;
   } 
+}
+
+export class UpdateJobVacancyRepositorySpy implements UpdateJobVacancyRepository {
+  result = true;
+  data: UpdateJobVacancy.Params | undefined;
+
+  async update(jobVacancyId: string, jobVacancyData: UpdateJobVacancy.Params): Promise<boolean>{
+    this.data = jobVacancyData;
+    return this.result;
+  }
+}
+
+const chance = new Chance();
+export class LoadJobVacancyByIdRepositorySpy implements LoadJobVacancyByIdRepository {
+  data = {
+    description: 'Vaga de Pedreiro',
+    announcerId: chance.guid(),
+    title: 'Pedreiro',
+    company: 'Votorantim',
+    salary: 56.800,
+    socialGroupsIds: [
+      chance.guid(),
+      chance.guid(),
+      chance.guid()
+    ],
+    deletedAt: new Date()
+  };
+  async loadById(id: string): Promise<LoadJobVacancyByIdRepository.Result>{
+    return {
+      id,
+      ...this.data
+    };
+  }
+  
 }
