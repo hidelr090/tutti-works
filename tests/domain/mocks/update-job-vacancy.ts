a12576857185135ff746333fe352cb14ff9dc444
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-04-29 12:43:57 -0300

    test: Create spies for db update job vacancy

---
+++ tests/domain/mocks/update-job-vacancy.ts
@@ -0,0 +1,18 @@
+import Chance from "chance";
+
+const chance = new Chance();
+
+export const mockUpdateJobVacancyParams = () => ({
+  id: chance.guid(),
+  description: 'Vaga de Pedreiro',
+  announcerId: chance.guid(),
+  title: 'Pedreiro',
+  company: 'Votorantim',
+  salary: 56.800,
+  socialGroupsIds: [
+    chance.guid(),
+    chance.guid(),
+    chance.guid()
+  ],
+  deletedAt: new Date()
+});
