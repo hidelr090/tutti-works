3047389968a764499f33d3d39f0dd7b9103837d3
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-12 14:05:15 -0300

    refactor: add avatarUrl to user interface

--- tests/domain/mocks/add-user.ts
+++ tests/domain/mocks/add-user.ts
@@ -9,7 +9,8 @@ export const mockAddUserParams = (): AddUser.Params => ({
   email: chance.email(),
   password: 'valid_password',
   identifierCode: chance.integer({min: 11, max: 11}).toString(),
-  phone: chance.phone()
+  phone: chance.phone(),
+  avatarUrl: chance.url().toString(),
 });
 
 //export const mockAuthenticationParams = (): Authentication.Params => ({});
