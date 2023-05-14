15567e9283efd3ef60e88dc47cb1137c99bae687
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-14 11:15:49 -0300

    feat: create AddRecruiterRepository

--- src/data/protocols/db/repositories/index.ts
+++ src/data/protocols/db/repositories/index.ts
@@ -2,3 +2,4 @@ export * from './user';
 export * from './job-vacancy';
 export * from './candidate-job-vacancy';
 export * from './candidate';
+export * from './recruiter';
---
+++ src/data/protocols/db/repositories/recruiter/add.ts
@@ -0,0 +1,10 @@
+import { AddRecruiter } from '@/domain/usecases';
+
+export interface AddRecruiterRepository {
+  add: (recruiterData: AddRecruiterRepository.Params) => Promise<AddRecruiterRepository.Result>;
+}
+
+export namespace AddRecruiterRepository {
+  export type Params = AddRecruiter.Params;
+  export type Result = boolean;
+}
