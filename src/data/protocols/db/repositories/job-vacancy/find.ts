c2d8c3f02c03801459e0a9b65690cfe7fd467678
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-14 12:29:34 -0300

    feat: create FindJobVacanciesRepository

---
+++ src/data/protocols/db/repositories/job-vacancy/find.ts
@@ -0,0 +1,10 @@
+import { FindJobVacancies } from "@/domain/usecases";
+
+export interface FindJobVacanciesRepository {
+  find: (data: FindJobVacanciesRepository.Params) => Promise<FindJobVacanciesRepository.Result>;
+}
+
+export namespace FindJobVacanciesRepository {
+  export type Params = FindJobVacancies.Params;
+  export type Result = FindJobVacancies.Result
+}
--- src/data/protocols/db/repositories/job-vacancy/index.ts
+++ src/data/protocols/db/repositories/job-vacancy/index.ts
@@ -1,3 +1,4 @@
 export * from './add';
 export * from './update';
 export * from './load-by-id';
+export * from './find';
