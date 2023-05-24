46327681123c7d865b268f9efd985584c0d2d1c7
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-24 09:02:44 -0300

    feat: add Auth Middleware

---
+++ src/presentation/middlewares/auth-middleware.ts
@@ -0,0 +1,26 @@
+import { HttpResponse, Middleware } from "@/presentation/protocols";
+import { forbidden, ok, serverError } from '@/presentation/helpers';
+import { AccessDeniedError } from "@/presentation/errors";
+import { LoadUserByToken } from '@/domain/usecases';
+
+export class AuthMiddleware implements Middleware {
+  constructor(
+    private readonly loadUserByToken: LoadUserByToken,
+    private readonly role?: string
+  ){}
+
+  async handle (httpRequest: any): Promise<HttpResponse>{
+    try{
+      const { accessToken } = httpRequest;
+      if(accessToken){
+        const user = await this.loadUserByToken.load(accessToken);
+        if (user)
+          return ok({ userId: user.id });
+      }
+      return forbidden(new AccessDeniedError());
+    } catch (error){
+      const err: Error = error as Error;
+      return serverError(err);
+    }
+  }
+}
