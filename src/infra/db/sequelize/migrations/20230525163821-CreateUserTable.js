e780b882995ef34273c9a52a44584bd0f742b028
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-26 11:26:13 -0300

    feat: add migration for UserTable

---
+++ src/infra/db/sequelize/migrations/20230525163821-CreateUserTable.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('user',{
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: null
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+      },
+      identifierCode: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+      },
+      birthDate: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: null
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+      },
+      avatarUrl: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+      },
+    })
+  },
+
+  async down (queryInterface) {
+    await queryInterface.dropTable('user');
+  }
+};
