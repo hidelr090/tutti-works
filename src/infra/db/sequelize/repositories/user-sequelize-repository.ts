07703d00f0987c405210a507558e1ff4f4374314
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-27 13:40:58 -0300

    feat: add implementation for loadByIdrepository

--- src/infra/db/sequelize/repositories/user-sequelize-repository.ts
+++ src/infra/db/sequelize/repositories/user-sequelize-repository.ts
@@ -1,7 +1,7 @@
-import { AddUserRepository, CheckUserByEmailRepository, LoadUserByEmailRepository } from "@/data";
+import { AddUserRepository, CheckUserByEmailRepository, LoadUserByEmailRepository, LoadUserByIdRepository } from "@/data";
 import { UserSequelizeModel } from "@/infra/db/sequelize/models";
 
-export class UserSequelizeRepository implements AddUserRepository, LoadUserByEmailRepository, CheckUserByEmailRepository {
+export class UserSequelizeRepository implements AddUserRepository, LoadUserByEmailRepository, CheckUserByEmailRepository, LoadUserByIdRepository{
   async add (data: AddUserRepository.Params): Promise<AddUserRepository.Result> {
     const user = await UserSequelizeModel.create(data);
     return user !== null;
@@ -29,5 +29,10 @@ export class UserSequelizeRepository implements AddUserRepository, LoadUserByEma
     const user = await UserSequelizeModel.findOne({ where: { email } });
 
     return user !== null;
+  }
+
+  async loadById (id: string): Promise<LoadUserByIdRepository.Result>{
+    const user = await UserSequelizeModel.findOne({ where: { id } });
+    return user ? user: null;
   }
 }
