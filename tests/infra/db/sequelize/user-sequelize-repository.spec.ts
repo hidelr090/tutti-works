482c61c1dbbe4bd97d502d6477ff0a9c3f6155a9
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-26 22:42:04 -0300

    test: ensure that UserSequelizeRepository.add returns true on success

---
+++ tests/infra/db/sequelize/user-sequelize-repository.spec.ts
@@ -0,0 +1,31 @@
+import { UserSequelizeRepository } from "@/infra/db/sequelize/repositories";
+import { mockAddUserParams } from "@/tests/domain/mocks";
+import { sequelize } from "@/infra/db/config/sequelize";
+
+const makeSut = (): UserSequelizeRepository => {
+  return new UserSequelizeRepository();
+};
+
+describe('UserSequelizeRepository', () => {
+  beforeAll(async () => {
+    await sequelize.authenticate();
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  describe('add()', () => {
+    test('Should return true on success', async () => {
+      try{
+        const sut = makeSut();
+        const addUserParams = mockAddUserParams();
+        const isValid = await sut.add(addUserParams);
+        expect(isValid).toBe(true);
+      }catch(err){
+        console.error(err);
+        throw err;
+      }
+    });
+  });
+});
