54e8c89218c1478fee8ef4e95b71e07ceb5ba283
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-26 22:39:37 -0300

    feat: add Base model for entities

--- src/infra/db/sequelize/models/base.ts
+++ src/infra/db/sequelize/models/base.ts
@@ -1,17 +1,26 @@
-import { Table, Column, PrimaryKey} from "sequelize-typescript";
+import { DataTypes } from 'sequelize';
+import { v4 } from 'uuid';
 
-@Table
-export class Base {
-  @PrimaryKey
-  @Column
-  id?: string;
-
-  @Column
-  createdAt?: Date;
-
-  @Column
-  deletedAt?: Date;
-
-  @Column
-  updatedAt?: Date;
-}
+export const BaseModel = {
+  id: {
+    type: DataTypes.UUIDV4,
+    defaultValue: v4(),
+    allowNull: false,
+    primaryKey: true
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: new Date(),
+    allowNull: true,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: new Date(),
+    allowNull: true,
+  },
+  deletedAt: {
+    type: DataTypes.DATE,
+    defaultValue: null,
+    allowNull: true,
+  },
+};
