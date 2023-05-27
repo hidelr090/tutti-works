10b148bc7eba6ae46ea299a28b1017a906d7aa8f
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-27 05:30:50 -0300

    feat: implementation of User loadByEmail

--- src/infra/db/sequelize/repositories/user-sequelize-repository.ts
+++ src/infra/db/sequelize/repositories/user-sequelize-repository.ts
@@ -1,10 +1,28 @@
-import { AddUserRepository } from "@/data";
-import { User } from "@/infra/db/sequelize/models";
+import { AddUserRepository, LoadUserByEmailRepository } from "@/data";
+import { UserSequelizeModel } from "@/infra/db/sequelize/models";
 
-export class UserSequelizeRepository implements AddUserRepository {
+export class UserSequelizeRepository implements AddUserRepository, LoadUserByEmailRepository {
   async add (data: AddUserRepository.Params): Promise<AddUserRepository.Result> {
-    const user = await User.create(data);
+    const user = await UserSequelizeModel.create(data);
     return user !== null;
+  }
+
+  async loadByEmail (email: string): Promise<LoadUserByEmailRepository.Result>{
+    const user = await UserSequelizeModel.findOne({ where: { email } });
+
+    if(!user){
+      throw new Error(`Email ${email} not found`);
+    }
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      phone: user.phone,
+      avatarUrl: user.avatarUrl,
+      identifierCode: user.identifierCode,
+      password: user.password,
+    };
   }
 
 }
