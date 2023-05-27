d42c388c359322880bc5e41a51bbf8c982d6560f
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-27 05:31:20 -0300

    test: ensure that LoadByEmail returns a user on sucess

--- tests/infra/db/sequelize/user-sequelize-repository.spec.ts
+++ tests/infra/db/sequelize/user-sequelize-repository.spec.ts
@@ -1,6 +1,7 @@
 import { UserSequelizeRepository } from "@/infra/db/sequelize/repositories";
 import { mockAddUserParams } from "@/tests/domain/mocks";
 import { sequelize } from "@/infra/db/config/sequelize";
+import { UserSequelizeModel } from "@/infra/db/sequelize/models";
 
 const makeSut = (): UserSequelizeRepository => {
   return new UserSequelizeRepository();
@@ -15,12 +16,29 @@ describe('UserSequelizeRepository', () => {
     await sequelize.close();
   });
 
+  beforeEach(async () => {
+    await UserSequelizeModel.destroy({truncate: true});
+  });
+
   describe('add()', () => {
     test('Should return true on success', async () => {
       const sut = makeSut();
       const addUserParams = mockAddUserParams();
       const isValid = await sut.add(addUserParams);
       expect(isValid).toBe(true);
+    });
+  });
+
+  describe('loadByEmail()', () => {
+    test('Should return a user on success', async () => {
+      const sut = makeSut();
+      const addUserParams = mockAddUserParams();
+      await sut.add(addUserParams);
+      const user = await sut.loadByEmail(addUserParams.email);
+      expect(user).toBeTruthy();
+      expect(user.id).toBeTruthy();
+      expect(user.name).toBe(addUserParams.name);
+      expect(user.password).toBe(addUserParams.password);
     });
   });
 });
