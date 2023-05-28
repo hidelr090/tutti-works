6adff4ead994e3aa5a1962e3be016e9ebe4ea8e5
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-28 12:16:45 -0300

    feat: add CreateHistoryTable migration

---
+++ src/infra/db/sequelize/migrations/20230528151305-CreateHistoryTable.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('history', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: null
+      },
+      userId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+      },
+      start: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: null,
+      },
+      end: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: null,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+      }
+    });
+  },
+
+  async down (queryInterface) {
+    await queryInterface.dropTable('history');
+  }
+};
