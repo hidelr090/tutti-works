c9418d7fd99c747ec72d260cdbe55a4a68a128ac
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-28 14:19:40 -0300

    feat: add loadHistoryByIdRepository implementation

--- src/infra/db/sequelize/repositories/history-sequelize-repository.ts
+++ src/infra/db/sequelize/repositories/history-sequelize-repository.ts
@@ -2,10 +2,14 @@ import { AddHistoryRepository, LoadHistoryByIdRepository } from '@/data/protocol
 import { AddHistory } from '@/domain/usecases';
 import { HistorySequelizeModel } from '@/infra/db/sequelize/models';
 
-export class HistorySequelizeRepository implements AddHistoryRepository{
+export class HistorySequelizeRepository implements AddHistoryRepository, LoadHistoryByIdRepository{
   async add (historyData: AddHistory.Params): Promise<boolean>{
     const history = await HistorySequelizeModel.create(historyData);
     return history !== null;
   }
 
+  async loadById(id: string): Promise<LoadHistoryByIdRepository.Result>{
+    const history = await HistorySequelizeModel.findOne({where: { id }});
+    return history; 
+  } 
 }
