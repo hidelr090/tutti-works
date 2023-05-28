3a91599295a8730b35de0e3f726eaf64ccd1e74e
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-28 11:26:52 -0300

    feat: add CreateCandidateTable migration

---
+++ src/infra/db/sequelize/migrations/20230528142136-CreateCandidateTable.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('candidate',{
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: null
+      },
+      userId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+      }
+    })
+  },
+
+  async down (queryInterface) {
+    await queryInterface.dropTable('candidate');
+  }
+};
