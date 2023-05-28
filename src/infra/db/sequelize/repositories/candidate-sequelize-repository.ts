c569dd99f83df3ec02b79ba03b085d9e875239ad
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-28 11:56:59 -0300

    feat: add implementation for checkByUserIdRepository

--- src/infra/db/sequelize/repositories/candidate-sequelize-repository.ts
+++ src/infra/db/sequelize/repositories/candidate-sequelize-repository.ts
@@ -1,11 +1,16 @@
-import { AddCandidateRepository } from '@/data/protocols/db/repositories';
+import { AddCandidateRepository, CheckCandidateByUserIdRepository } from '@/data/protocols/db/repositories';
 import { CandidateSequelizeModel } from '@/infra/db/sequelize/models';
 
-export class CandidateSequelizeRepository implements AddCandidateRepository {
+export class CandidateSequelizeRepository implements AddCandidateRepository, CheckCandidateByUserIdRepository {
   async add(candidateData: AddCandidateRepository.Params) : Promise<AddCandidateRepository.Result>{
     const candidate = await CandidateSequelizeModel.create(candidateData);
     return candidate !== null;
   }
 
+  async checkByUserId (userId: string): Promise<boolean>{
+    const candidate = await CandidateSequelizeModel.findOne({ where: { userId }});
+    return candidate !== null;
+  }
 
+  
 }
