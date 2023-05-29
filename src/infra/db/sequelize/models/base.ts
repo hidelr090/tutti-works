dc7e7460241fc6398e51539d721f54e980ab4b82
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-29 19:37:11 -0300

    refactor: set default value for columns on basemodel

--- src/infra/db/sequelize/models/base.ts
+++ src/infra/db/sequelize/models/base.ts
@@ -4,18 +4,18 @@ import { v4 } from 'uuid';
 export const BaseModel = {
   id: {
     type: DataTypes.UUIDV4,
-    defaultValue: v4(),
+    defaultValue: () => v4(),
     allowNull: false,
     primaryKey: true
   },
   createdAt: {
     type: DataTypes.DATE,
-    defaultValue: new Date(),
+    defaultValue: () => new Date(),
     allowNull: true,
   },
   updatedAt: {
     type: DataTypes.DATE,
-    defaultValue: new Date(),
+    defaultValue: () => new Date(),
     allowNull: true,
   },
   deletedAt: {
