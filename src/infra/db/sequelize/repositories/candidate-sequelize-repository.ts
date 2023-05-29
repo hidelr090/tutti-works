41b323d2a68e7935b5fee432bbe1fae6fa8e6414
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-29 19:39:23 -0300

    feat: add implementation for findCandidateByUserId repository

--- src/infra/db/sequelize/repositories/candidate-sequelize-repository.ts
+++ src/infra/db/sequelize/repositories/candidate-sequelize-repository.ts
@@ -1,7 +1,13 @@
-import { AddCandidateRepository, CheckCandidateByUserIdRepository } from '@/data/protocols/db/repositories';
-import { CandidateSequelizeModel } from '@/infra/db/sequelize/models';
+import { AddCandidateRepository, CheckCandidateByUserIdRepository, FindCandidateByUserIdRepository } from '@/data/protocols/db/repositories';
+import { ShowCandidateProfile } from '@/domain/usecases';
+import { Candidate, CandidateSequelizeModel, User, History } from '@/infra/db/sequelize/models';
 
-export class CandidateSequelizeRepository implements AddCandidateRepository, CheckCandidateByUserIdRepository {
+Candidate.associate();
+User.associate();
+History.associate();
+
+export class CandidateSequelizeRepository implements AddCandidateRepository, CheckCandidateByUserIdRepository, FindCandidateByUserIdRepository {
+
   async add(candidateData: AddCandidateRepository.Params) : Promise<AddCandidateRepository.Result>{
     const candidate = await CandidateSequelizeModel.create(candidateData);
     return candidate !== null;
@@ -12,5 +18,45 @@ export class CandidateSequelizeRepository implements AddCandidateRepository, Che
     return candidate !== null;
   }
 
-  
+  async findByUserId (userId: string): Promise<ShowCandidateProfile.Result>{
+    const candidate = await CandidateSequelizeModel.findOne({ 
+      where: { userId }, 
+      include: [ 
+        { 
+          model: User, 
+          as:'user', 
+          include: 
+          [
+            { model: History, as: 'histories' }
+          ]
+        }
+      ]
+    }) as Candidate;
+
+    const user = candidate?.user as User;
+    const histories = user?.histories as History[];
+
+    return {
+      user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          identifierCode: user.identifierCode,
+          phone: user.phone,
+          avatarUrl: user.avatarUrl,
+      },
+      candidate: {
+          id: candidate.id,
+          description: candidate.description,
+          role: candidate.role
+      },
+      history: histories.map(history => ({
+          id: history.id,
+          name:  history.name,
+          start: history.start,
+          end: history.end,
+          description: history.description
+      }))
+    }
+  }
 }
