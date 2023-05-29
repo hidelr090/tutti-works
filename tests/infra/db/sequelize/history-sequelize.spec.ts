7f0ab7a803744c076fcd906760ed60dd13fd3f3b
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-05-29 12:02:33 -0300

    test: ensure that LoadHistoryByIdRepository returns a history on succes

--- tests/infra/db/sequelize/history-sequelize.spec.ts
+++ tests/infra/db/sequelize/history-sequelize.spec.ts
@@ -2,6 +2,7 @@ import { HistorySequelizeRepository } from "@/infra/db/sequelize/repositories";
 import { mockAddHistoryParams } from '@/tests/domain/mocks';
 import { sequelize } from "@/infra/db/config/sequelize";
 import { HistorySequelizeModel } from "@/infra/db/sequelize/models";
+import { LoadHistoryByIdRepository } from "@/data";
 
 const makeSut = (): HistorySequelizeRepository => {
   return new HistorySequelizeRepository();
@@ -27,5 +28,18 @@ describe('HistorySequelizeRepository', () => {
       const isValid = await sut.add(addHistoryParams);
       expect(isValid).toBeTruthy();
     });
+  });
+
+  describe('loadById()', () => {
+    test('Sould return a history object', async ()=> {
+      const sut = makeSut();
+      const addHistoryParams = mockAddHistoryParams();
+      await sut.add(addHistoryParams);
+      const loaded = await HistorySequelizeModel.findOne({where: {userId: addHistoryParams.userId }}) as LoadHistoryByIdRepository.Result;
+      const history = await sut.loadById(loaded?.id as string);
+      expect(history).toBeTruthy();
+      expect(history).toEqual({...loaded});
+    });
+
   });
 });
