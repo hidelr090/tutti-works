2066cffa7a1c92f5ccc6bb933ea2e3112001c9da
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-01 12:34:34 -0300

    feat: add CandidateSocialGroups Db usecase

--- src/data/usecases/db-add-candidate-social-groups.ts
+++ src/data/usecases/db-add-candidate-social-groups.ts
@@ -1,18 +1,20 @@
 import { AddCandidateSocialGroups } from "@/domain/usecases";
-import { AddCandidateSocialGroupsRepository, CheckExistingSocialGroupsRepository} from "@/data/protocols/db/repositories";
+import { AddCandidateSocialGroupsRepository, FindSocialGroupsByCandidateIdRepository} from "@/data/protocols/db/repositories";
 
 export class DbAddCandidateSocialGroups implements AddCandidateSocialGroups {
   constructor(
     private readonly addCandidateSocialGroupsRepository: AddCandidateSocialGroupsRepository,
-    private readonly checkExistingSocialGroupsRepository: CheckExistingSocialGroupsRepository
+    private readonly findSocialGroupsByCandidateIdRepository: FindSocialGroupsByCandidateIdRepository
   ){}
 
   async add (candidateId: string, socialGroupsIds: string[]): Promise<boolean>{
     let result = false;
     
-    const existing = await this.checkExistingSocialGroupsRepository.checkExisting(candidateId, socialGroupsIds);
+    const existing = await this.findSocialGroupsByCandidateIdRepository.findByCandidateId(candidateId);
     
-    if(!existing)
+    const socialGroupsToInsert = socialGroupsIds.filter(item => !existing.includes(item));
+
+    if(socialGroupsToInsert && socialGroupsToInsert.length > 0)
       result = await this.addCandidateSocialGroupsRepository.add(candidateId, socialGroupsIds);
 
     return result;
