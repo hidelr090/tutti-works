562730783d3b3bcf6b600879ebaf72ffae46313b
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-01 12:37:44 -0300

    feat: add SocialGroupRepository

--- src/infra/db/sequelize/repositories/candidate-sequelize-repository.ts
+++ src/infra/db/sequelize/repositories/candidate-sequelize-repository.ts
@@ -1,12 +1,13 @@
-import { AddCandidateRepository, CheckCandidateByUserIdRepository, FindCandidateByUserIdRepository } from '@/data/protocols/db/repositories';
-import { ShowCandidateProfile } from '@/domain/usecases';
-import { Candidate, CandidateSequelizeModel, User, History } from '@/infra/db/sequelize/models';
+import { AddCandidateRepository, CheckCandidateByUserIdRepository, FindCandidateByUserIdRepository, ListCandidatesByRoleAndSocialGroupsRepository } from '@/data/protocols/db/repositories';
+import { SearchForCandidates, ShowCandidateProfile } from '@/domain/usecases';
+import { Candidate, CandidateSequelizeModel, User, History, SocialGroup, CandidateSocialGroupSequelizeModel, CandidateSocialGroup } from '@/infra/db/sequelize/models';
+import { Op } from 'sequelize';
 
 Candidate.associate();
+SocialGroup.associate();
 User.associate();
 History.associate();
-
-export class CandidateSequelizeRepository implements AddCandidateRepository, CheckCandidateByUserIdRepository, FindCandidateByUserIdRepository {
+export class CandidateSequelizeRepository implements AddCandidateRepository, CheckCandidateByUserIdRepository, FindCandidateByUserIdRepository, ListCandidatesByRoleAndSocialGroupsRepository{
 
   async add(candidateData: AddCandidateRepository.Params) : Promise<AddCandidateRepository.Result>{
     const candidate = await CandidateSequelizeModel.create(candidateData);
@@ -59,4 +60,59 @@ export class CandidateSequelizeRepository implements AddCandidateRepository, Che
       }))
     }
   }
+
+  async listByRoleAndSocialGroups (jobInfos: SearchForCandidates.Params): Promise<SearchForCandidates.Result>{
+    
+    const candidates = await CandidateSequelizeModel.findAll({
+      where: {
+        role: {
+          [Op.like]: `%${jobInfos.jobInfos}%`,
+        },
+      },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          include: [
+            {
+              model: History,
+              as: 'histories',
+            }
+          ]
+        },
+        {
+          model: SocialGroup,
+          as: 'socialGroups',
+          where: {
+            id: {
+              [Op.in]: jobInfos.socialGroupsIds
+            }
+          }
+        }
+      ]
+    });
+
+    return candidates.map(candidate => ({
+      user: {
+        id: candidate.user.id,
+        name: candidate.user.name,
+        email: candidate.user.email,
+        identifierCode: candidate.user.identifierCode,
+        phone: candidate.user.phone,
+        avatarUrl: candidate.user.avatarUrl
+      },
+      candidate: {
+        id: candidate.id,
+        description: candidate.description,
+        role: candidate.role
+      },
+      history: candidate.user.histories.map(history => ({
+        id: history.id,
+        name: history.name,
+        start: history.start,
+        end: history.end,
+        description: history.description
+      }))
+    }));
+  } 
 }
---
+++ src/infra/db/sequelize/repositories/social-group-sequelize-repository.ts
@@ -0,0 +1,21 @@
+import { FindSocialGroupsByCandidateIdRepository, ListSocialGroupsRepository } from '@/data/protocols/db/repositories';
+import { ListSocialGroups } from '@/domain/usecases';
+import { SocialGroupSequelizeModel } from '@/infra/db/sequelize/models';
+
+export class SocialGroupSequelizeRepository implements ListSocialGroupsRepository, FindSocialGroupsByCandidateIdRepository {
+  async list () :Promise<ListSocialGroups.Result>{
+    const result = await SocialGroupSequelizeModel.findAll();
+
+    return result.map(result => ({
+      id: result.id,
+      title: result.title,
+      description: result.description
+    })) || null;
+  }
+
+  async findByCandidateId(candidateId: string): Promise<FindSocialGroupsByCandidateIdRepository.Result>{
+    const found = await SocialGroupSequelizeModel.findAll({where: { candidateId }});
+
+    return found.map(item => item.id);
+  }
+}
