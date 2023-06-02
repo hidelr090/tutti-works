020547a132196ddadff3b53280f159183362569c
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-02 15:42:25 -0300

    feat: add implementation for FindJobVacanciesRepository

--- src/infra/db/sequelize/repositories/job-vacancy-sequelize-repository.ts
+++ src/infra/db/sequelize/repositories/job-vacancy-sequelize-repository.ts
@@ -1,14 +1,57 @@
 import { AddJobVacancyRepository, FindJobVacanciesRepository, LoadJobVacancyByIdRepository, UpdateJobVacancyRepository} from "@/data";
-import { FindJobVacancies, UpdateJobVacancy, ListRecruiterJobVacancies } from '@/domain/usecases';
-import { JobVacancy, JobVacancySequelizeModel } from "@/infra/db/sequelize/models";
+import { JobVacancy, JobVacancySequelizeModel, SocialGroup } from "@/infra/db/sequelize/models";
+import { Op } from "sequelize";
 
 JobVacancy.associate();
-
-export class JobVacancySequelizeRepository implements AddJobVacancyRepository {
+export class JobVacancySequelizeRepository implements AddJobVacancyRepository, FindJobVacanciesRepository{
   async add(jobVacancyData: AddJobVacancyRepository.Params): Promise<boolean>{
 
     const jobVacancy = await JobVacancySequelizeModel.create(jobVacancyData);
     
     return jobVacancy !==null;
+  }
+
+  async find(data: FindJobVacanciesRepository.Params): Promise<FindJobVacanciesRepository.Result>{
+    const jobVacancies = await JobVacancySequelizeModel.findAll({
+      where: {
+        [Op.or]:{
+          title: {
+            [Op.like]: `%${data.jobInfos}%`,
+          },
+          description: { 
+            [Op.like]: `%${data.jobInfos}%`,
+          }
+        }
+      },
+      include:[
+        {
+          model: SocialGroup,
+          as: 'socialGroups',
+          where: {
+            id: {
+              [Op.in]: data.socialGroupsIds
+            }
+          }
+        }
+      ]
+    });
+
+    let result: FindJobVacanciesRepository.Result = null;
+    
+    if(jobVacancies) {
+      result = jobVacancies.map(item => ({
+        description: item.description,
+        recruiterId: item.recruiterId,
+        title: item.title,
+        company: item.company,
+        wage: item.wage,
+        socialGroups: item.socialGroups.map(inner => ({
+          id: inner.id,
+          title: inner.title,
+        }))
+      }));
+    }
+
+    return result;
   }
 }
