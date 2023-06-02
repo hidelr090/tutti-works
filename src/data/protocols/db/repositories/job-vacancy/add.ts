2280631db1af9c83404ed3f981efcfdefb4d61a5
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-02 13:32:38 -0300

    feat: remove socialgroupsids from addjobvacancy params

--- src/data/protocols/db/repositories/job-vacancy/add.ts
+++ src/data/protocols/db/repositories/job-vacancy/add.ts
@@ -1,10 +1,15 @@
-import { AddJobVacancy } from "@/domain/usecases";
-
 export interface AddJobVacancyRepository {
   add: (jobVacancyData: AddJobVacancyRepository.Params) => Promise<AddJobVacancyRepository.Result>;
 }
 
 export namespace AddJobVacancyRepository {
-  export type Params = AddJobVacancy.Params;
+  export type Params = {
+    id: string;
+    description: string;
+    recruiterId: string;
+    title: string;
+    company: string;
+    wage: number;
+  };
   export type Result = boolean;
 }
