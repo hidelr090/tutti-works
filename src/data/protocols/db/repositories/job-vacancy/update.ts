574e42fdca05ef32dd426d81def9aa48920ecbee
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-02 16:19:20 -0300

    refactor: update Params for UpdateJobVacancy

--- src/data/protocols/db/repositories/job-vacancy/update.ts
+++ src/data/protocols/db/repositories/job-vacancy/update.ts
@@ -1,10 +1,15 @@
-import { UpdateJobVacancy } from "@/domain/usecases";
-
 export interface UpdateJobVacancyRepository {
   update: (jobVacancyId: string, jobVacancyData: UpdateJobVacancyRepository.Params) => Promise<UpdateJobVacancyRepository.Result>;
 }
 
 export namespace UpdateJobVacancyRepository {
-  export type Params = UpdateJobVacancy.Params;
+  export type Params = {
+    description: string;
+    recruiterId: string;
+    title: string;
+    company: string;
+    wage: number;
+    deletedAt: Date;
+  };
   export type Result = boolean;
 }
