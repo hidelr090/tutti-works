102d03b00e9504141815e524e2e65f9acaaabbcb
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-02 17:28:11 -0300

    feat: add model for CandidateJobVacancy

---
+++ src/infra/db/sequelize/models/candidate-job-vacancy.ts
@@ -0,0 +1,45 @@
+import { sequelize } from "@/infra/db/config/sequelize";
+import { BaseModel } from "./base";
+import { DataTypes, Model } from 'sequelize';
+
+export class CandidateJobVacancy extends Model {
+  public id!: string;
+  public deletedAt!: Date;
+  public candidateId!: string;
+  public jobVacancyId!: string;
+
+  public static associate: () => void;
+}
+
+CandidateJobVacancy.init({
+  ...BaseModel,
+  candidateId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    references: {
+      model: 'candidate',
+      key: 'id'
+    }
+  },
+  jobVacancyId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    references: {
+      model: 'social-group',
+      key: 'id'
+    }
+  },
+  resumeUrl: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  }
+}, {
+  sequelize,
+  tableName: 'candidate-social-group'
+});
+
+type CandidateJobVacancyModelStatic = typeof Model & {
+  new (): CandidateJobVacancy;
+}
+
+export const CandidateJobVacancySequelizeModel = CandidateJobVacancy as CandidateJobVacancyModelStatic;
