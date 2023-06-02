46dbe7d0581aca7a53005cbe5f602eb61fcfefe9
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-02 16:20:10 -0300

    feat: update JobVacancySocialGroups relations when updates a JobVacancy

--- src/data/usecases/db-update-job-vacancy.ts
+++ src/data/usecases/db-update-job-vacancy.ts
@@ -1,21 +1,29 @@
 import { UpdateJobVacancy } from "@/domain/usecases";
-import { UpdateJobVacancyRepository } from "@/data/protocols/db/repositories";
+import { UpdateJobVacancyRepository, DeleteJobVacancySocialGroupRepository} from "@/data/protocols/db/repositories";
 import { LoadJobVacancyByIdRepository } from "@/data/protocols/db/repositories";
-
 export class DbUpdateJobVacancy implements UpdateJobVacancy {
   constructor(
     private readonly updateJobVacancyRepository: UpdateJobVacancyRepository,
-    private readonly loadJobVacancyByIdRepository: LoadJobVacancyByIdRepository
+    private readonly loadJobVacancyByIdRepository: LoadJobVacancyByIdRepository,
+    private readonly deleteJobVacancySocialGroupRepository: DeleteJobVacancySocialGroupRepository
   ){}
 
   async update(jobVacancyId: string, data: UpdateJobVacancy.Params): Promise<UpdateJobVacancy.Result> {
-
     const vacancyToUpdate = await this.loadJobVacancyByIdRepository.loadById(jobVacancyId);
     
     let result = false;
 
-    if(vacancyToUpdate){
+    if (vacancyToUpdate) {
       await this.updateJobVacancyRepository.update(jobVacancyId, { ...vacancyToUpdate, ...data });
+
+      const socialGroups = vacancyToUpdate.socialGroupsIds;
+
+      for (const item of socialGroups) {
+        if (!data.socialGroupsIds.includes(item)) {
+          await this.deleteJobVacancySocialGroupRepository.delete(jobVacancyId, item);
+        }
+      }
+
       result = true;
     }
 
