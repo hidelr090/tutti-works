d80acb1bd938f34f19601d83db1d556707c0338f
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-05 11:26:59 -0300

    feat: add AddJobVacancyController

---
+++ src/presentation/controllers/add-job-vacancy-controller.ts
@@ -0,0 +1,36 @@
+import { Controller, HttpResponse, Validation} from '@/presentation/protocols';
+import { badRequest, noContent, serverError } from '@/presentation/helpers';
+import { AddJobVacancy } from '@/domain/usecases';
+
+export class AddJobVacancyController implements Controller {
+  constructor (
+    private readonly validation: Validation,
+    private readonly addJobVacancy: AddJobVacancy
+  ){}
+
+  async handle(request: AddJobVacancyController.Request): Promise<HttpResponse>{
+    try{
+      const error = this.validation.validate(request);
+      if(error)
+        return badRequest(error);
+
+      await this.addJobVacancy.add(request);
+
+      return noContent();
+
+    }catch(err){
+      return serverError(err as Error);
+    }
+  }
+}
+
+export namespace AddJobVacancyController {
+  export type Request = {
+    description: string;
+    recruiterId: string;
+    title: string;
+    company: string;
+    wage: number;
+    socialGroupsIds: string[];
+  }
+}
