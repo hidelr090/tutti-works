adc67b3eea51a01c6a8f79a2da35c72d3a8493f6
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-05 11:26:47 -0300

    feat: add AddHistoryController

---
+++ src/presentation/controllers/add-history-controller.ts
@@ -0,0 +1,35 @@
+import { Controller, HttpResponse, Validation} from '@/presentation/protocols';
+import { badRequest, noContent, serverError } from '@/presentation/helpers';
+import { AddHistory } from '@/domain/usecases';
+
+export class AddHistoryController implements Controller {
+  constructor(
+    private readonly validation: Validation, 
+    private readonly addHistory: AddHistory
+  ){}
+
+  async handle (request: AddHistoryController.Request): Promise<HttpResponse>{
+    try {
+      const error = this.validation.validate(request);
+      if(error)
+        return badRequest(error);
+
+      await this.addHistory.add(request);
+
+      return noContent();
+
+    }catch(err){
+      return serverError(err as Error);
+    }
+  }
+}
+
+export namespace AddHistoryController {
+  export type Request = {
+    name: string;
+    start: Date;
+    end: Date;
+    description: string;
+    userId: string;
+  }
+}
