8bd76a8d662f042ad13a6032eb9c0b9e127c6cf4
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-05 11:26:36 -0300

    feat: add AddCandidateSocialGroupController

---
+++ src/presentation/controllers/add-candidate-social-group-controller.ts
@@ -0,0 +1,32 @@
+import { Controller, HttpResponse, Validation} from '@/presentation/protocols';
+import { badRequest, noContent, serverError } from '@/presentation/helpers';
+import { AddCandidateSocialGroups } from '@/domain/usecases';
+
+export class AddCandidateSocialGroupsController implements Controller {
+  constructor (
+    private readonly validation: Validation,
+    private readonly addCandidateSocialGroupAddCandidateSocialGroups: AddCandidateSocialGroups
+  ){}
+
+  async handle(request: AddCandidateSocialGroupsController.Request): Promise<HttpResponse>{
+    try{
+      const error = this.validation.validate(request);
+      if(error)
+        return badRequest(error);
+
+      await this.addCandidateSocialGroupAddCandidateSocialGroups.add(request.candidateId, request.socialGroupsIds);
+
+      return noContent();
+
+    }catch(err){
+      return serverError(err as Error);
+    }
+  }
+}
+
+export namespace AddCandidateSocialGroupsController {
+  export type Request = {
+    candidateId: string; 
+    socialGroupsIds: string[];
+  }
+}
