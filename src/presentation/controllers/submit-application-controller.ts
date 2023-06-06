71af75714da8c4c7313354040bca27a9ab0a71ba
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-05 22:08:10 -0300

    feat: add SubmitApplicationController

---
+++ src/presentation/controllers/submit-application-controller.ts
@@ -0,0 +1,34 @@
+import { Controller, HttpResponse, Validation} from '@/presentation/protocols';
+import { badRequest, noContent, serverError } from '@/presentation/helpers';
+import { SubmitApplication } from '@/domain/usecases';
+
+export class SubmitApplicationController implements Controller {
+  constructor (
+    private readonly validation: Validation,
+    private readonly submitApplication: SubmitApplication
+  ){}
+
+  async handle(request: SubmitApplicationController.Request): Promise<HttpResponse>{
+    try{
+      const error = this.validation.validate(request);
+      
+      if(error)
+        return badRequest(error);
+
+      await this.submitApplication.submitApplication(request);
+
+      return noContent();
+
+    }catch(err){
+      return serverError(err as Error);
+    }
+  }
+}
+
+export namespace SubmitApplicationController {
+  export type Request = {
+    candidateId: string,
+    jobVacancyId: string,
+    resumeUrl: string
+  };
+}
