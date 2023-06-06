a1acfe842902d3cb109d1dc958241e96d2289b62
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-06 16:54:18 -0300

    feat: add UpdateJobVacancyFactory

---
+++ src/main/factories/usecases/update-job-vacancy-factory.ts
@@ -0,0 +1,9 @@
+import { DbUpdateJobVacancy } from "@/data";
+import { UpdateJobVacancy } from "@/domain/usecases";
+import { JobVacancySequelizeRepository, JobVacancySocialGroupSequelizeRepository } from "@/infra/db/sequelize/repositories";
+
+export const makeDbUpdateJobVacancy = (): UpdateJobVacancy => {
+  const jobVacancyRepository = new JobVacancySequelizeRepository();
+  const jobVacancySocialGroupRepository = new JobVacancySocialGroupSequelizeRepository();
+  return new DbUpdateJobVacancy(jobVacancyRepository, jobVacancyRepository, jobVacancySocialGroupRepository, jobVacancySocialGroupRepository);
+};
