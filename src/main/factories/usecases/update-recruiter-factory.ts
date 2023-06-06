58636fc028617ce092b3b1436dda6da45079367e
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-06 16:54:26 -0300

    feat: add UpdateRecruiterFactory

---
+++ src/main/factories/usecases/update-recruiter-factory.ts
@@ -0,0 +1,8 @@
+import { DbUpdateRecruiter } from "@/data";
+import { UpdateRecruiter } from "@/domain/usecases";
+import { RecruiterSequelizeRepository } from "@/infra/db/sequelize/repositories";
+
+export const makeDbUpdateRecruiter = (): UpdateRecruiter => {
+  const recruiterRepository = new RecruiterSequelizeRepository();
+  return new DbUpdateRecruiter(recruiterRepository, recruiterRepository);
+};
