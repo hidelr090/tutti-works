0e62fed85fe133c780549706075e47e5c3766bf3
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-06 14:30:07 -0300

    feat: add DbAddUserFactory

---
+++ src/main/factories/usecases/add-user-factory.ts
@@ -0,0 +1,11 @@
+import { DbAddUser } from "@/data";
+import { AddUser } from "@/domain/usecases";
+import { BcryptAdapter } from "@/infra/cryptography";
+import { UserSequelizeRepository } from "@/infra/db/sequelize/repositories";
+
+export const makeDbAddUser = (): AddUser => {
+  const salt = 12;
+  const bcryptAdapter = new BcryptAdapter(salt);
+  const userSequelizeRepository = new UserSequelizeRepository();
+  return new DbAddUser(bcryptAdapter, userSequelizeRepository, userSequelizeRepository, userSequelizeRepository);
+};
