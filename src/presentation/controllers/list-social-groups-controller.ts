0ab6120253d2226d1ed6541a9c5815a7f2c112b1
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-05 22:07:26 -0300

    feat: add ListSocialGroupsController

---
+++ src/presentation/controllers/list-social-groups-controller.ts
@@ -0,0 +1,23 @@
+import {Controller, HttpResponse} from '@/presentation/protocols';
+import {noContent, serverError, ok} from '@/presentation/helpers';
+import { ListSocialGroups } from '@/domain/usecases';
+
+export class ListSocialGroupsController implements Controller {
+  constructor(
+    private readonly listSocialGroups: ListSocialGroups
+  ){}
+
+  async handle(request: ListSocialGroupsController.Request): Promise<HttpResponse>{
+    try{
+      const socialGroups = await this.listSocialGroups.listSocialGroups();
+
+      return socialGroups && socialGroups.length ? ok(socialGroups) : noContent();
+    }catch(err){
+      return serverError(err as Error);
+    }
+  }
+}
+
+export namespace ListSocialGroupsController {
+  export type Request = null;
+}
