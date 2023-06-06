5c380647c93340045ee9cab018a4074d67bff51c
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-06 14:41:44 -0300

    feat: add implementation for submit application factory

---
+++ src/main/factories/usecases/index.ts
@@ -0,0 +1,2 @@
+export * from './add-user-factory';
+export * from './submit-application-factory';
---
+++ src/main/factories/usecases/submit-application-factory.ts
@@ -0,0 +1,8 @@
+import { DbAddCandidateJobVacancy } from "@/data";
+import { SubmitApplication } from "@/domain/usecases";
+import { CandidateJobVacancySequelizeRepository } from "@/infra/db/sequelize/repositories";
+
+export const makeDbAddCandidateJobVacancy = (): SubmitApplication => {
+  const candidateJobVacancyRepository = new CandidateJobVacancySequelizeRepository();
+  return new DbAddCandidateJobVacancy(candidateJobVacancyRepository);
+};
