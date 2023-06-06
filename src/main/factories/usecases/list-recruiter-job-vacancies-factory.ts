a118fb5ae459b41d02e44108ce78f0fad287b867
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-06 15:52:26 -0300

    feat: add ListRecruiterJobVacanciesFactory

---
+++ src/main/factories/usecases/list-recruiter-job-vacancies-factory.ts
@@ -0,0 +1,8 @@
+import { DbListRecruiterJobVacancies } from "@/data";
+import { ListRecruiterJobVacancies } from "@/domain/usecases";
+import { JobVacancySequelizeRepository } from "@/infra/db/sequelize/repositories";
+
+export const makeDbListRecruiterJobVacancies = (): ListRecruiterJobVacancies => {
+  const jobVacancyRepository = new JobVacancySequelizeRepository();
+  return new DbListRecruiterJobVacancies(jobVacancyRepository);
+};
