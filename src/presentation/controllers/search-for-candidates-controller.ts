4a3a955234018ef9fea27881918365b10a51098b
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-05 22:07:39 -0300

    feat: add SearchForCandidatesController

---
+++ src/presentation/controllers/search-for-candidates-controller.ts
@@ -0,0 +1,26 @@
+import {Controller, HttpResponse} from '@/presentation/protocols';
+import {noContent, serverError, ok} from '@/presentation/helpers';
+import { SearchForCandidates } from '@/domain/usecases';
+
+export class SearchForCandidatesController implements Controller {
+  constructor(
+    private readonly searchForCandidates: SearchForCandidates
+  ){}
+
+  async handle(request: SearchForCandidatesController.Request): Promise<HttpResponse>{
+    try{
+      const jobVacancies = await this.searchForCandidates.searchForCandidates(request);
+
+      return jobVacancies && jobVacancies.length ? ok(jobVacancies) : noContent();
+    }catch(err){
+      return serverError(err as Error);
+    }
+  }
+}
+
+export namespace SearchForCandidatesController {
+  export type Request = {
+    jobInfos: string;
+    socialGroupsIds: string[];
+  };
+}
