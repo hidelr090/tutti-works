cb197ea86ef8062c628557c2c85945b82245e6ed
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-06 17:12:41 -0300

    feat: add AddCandidateValidationFactory

---
+++ src/main/factories/controllers/add-candidate-validation-factory.ts
@@ -0,0 +1,10 @@
+import { ValidationComposite, RequiredFieldValidation } from '@/validation/validators'
+import { Validation } from '@/presentation/protocols'
+
+export const makeAddCandidateValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['userId', 'description', 'role']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  return new ValidationComposite(validations)
+}
