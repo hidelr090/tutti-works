5870d0d86a2e988962fdc688fcf7d20dbf89b001
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-06 14:39:39 -0300

    fix: fix class name of  CandidateJobVacancyRepository

--- src/infra/db/sequelize/repositories/candidate-job-vacancy-sequelize-repository.ts
+++ src/infra/db/sequelize/repositories/candidate-job-vacancy-sequelize-repository.ts
@@ -1,7 +1,7 @@
 import { AddCandidateJobVacancyRepository } from "@/data/protocols/db/repositories";
-import { CandidateJobVacancySequelizeModel, JobVacancy } from "@/infra/db/sequelize/models";
+import { CandidateJobVacancySequelizeModel } from "@/infra/db/sequelize/models";
 
-export class CandidateSocialGroupSequelizeRepository implements AddCandidateJobVacancyRepository{
+export class CandidateJobVacancySequelizeRepository implements AddCandidateJobVacancyRepository{
   async add (data: AddCandidateJobVacancyRepository.Params): Promise<boolean>{
     const added = await CandidateJobVacancySequelizeModel.create(data);
 
