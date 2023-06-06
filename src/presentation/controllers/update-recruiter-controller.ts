c800310a16c0c3a7ec593acf514371da94735c99
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-06 13:47:25 -0300

    feat: add UpdateRecruiterController

---
+++ src/presentation/controllers/update-recruiter-controller.ts
@@ -0,0 +1,32 @@
+import { Controller, HttpResponse, Validation} from '@/presentation/protocols';
+import { badRequest, noContent, serverError } from '@/presentation/helpers';
+import { UpdateRecruiter } from '@/domain/usecases';
+
+export class UpdateRecruiterController implements Controller {
+  constructor (
+    private readonly validation: Validation,
+    private readonly updateRecruiter: UpdateRecruiter
+  ){}
+
+  async handle(request: UpdateRecruiterController.Request): Promise<HttpResponse>{
+    try{
+      const error = this.validation.validate(request);
+      if(error)
+        return badRequest(error);
+
+      await this.updateRecruiter.update(request.userId, request);
+
+      return noContent();
+
+    }catch(err){
+      return serverError(err as Error);
+    }
+  }
+}
+
+export namespace UpdateRecruiterController {
+  export type Request = {
+    company: string,
+    userId: string,
+  }
+}
