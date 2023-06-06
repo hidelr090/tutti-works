bb47d0b24141597a2889edd74dfedab8fa8d2e67
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-06 16:54:39 -0300

    feat: add UpdateUserFactory

---
+++ src/main/factories/usecases/update-user-factory.ts
@@ -0,0 +1,8 @@
+import { DbUpdateUser } from "@/data";
+import { UpdateUser } from "@/domain/usecases";
+import { UserSequelizeRepository } from "@/infra/db/sequelize/repositories";
+
+export const makeDbUpdateUser = (): UpdateUser => {
+  const userRepository = new UserSequelizeRepository();
+  return new DbUpdateUser(userRepository, userRepository);
+};
