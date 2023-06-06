d145f26917c1efcb7ad6bcce62453044b1c938cb
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-06 13:47:47 -0300

    feat: add UpdateUserController

---
+++ src/presentation/controllers/update-user-controller.ts
@@ -0,0 +1,41 @@
+import { Controller, HttpResponse, Validation} from '@/presentation/protocols';
+import { badRequest, noContent, serverError } from '@/presentation/helpers';
+import { UpdateUser } from '@/domain/usecases';
+
+export class UpdateUserController implements Controller {
+  constructor (
+    private readonly validation: Validation,
+    private readonly updateUser: UpdateUser
+  ){}
+
+  async handle(request: UpdateUserController.Request): Promise<HttpResponse>{
+    try{
+      const error = this.validation.validate(request);
+      if(error)
+        return badRequest(error);
+
+      await this.updateUser.update(request.id, request);
+
+      return noContent();
+
+    }catch(err){
+      return serverError(err as Error);
+    }
+  }
+}
+
+export namespace UpdateUserController {
+  export type Request = {
+    id: string;
+    name?: string;
+    email?: string;
+    password?: string;
+    identifierCode?: string;
+    phone?: string;
+    description?: string;
+    linkedIn?: string;
+    github?: string;
+    instagram?: string;
+    accessToken?: string;
+  }
+}
