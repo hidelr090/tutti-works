786962721898491ebee76f31d7961028e257f593
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-05 22:06:55 -0300

    feat: add ListCandidateApplicationsController

---
+++ src/presentation/controllers/list-candidate-applications-controller.ts
@@ -0,0 +1,23 @@
+import {Controller, HttpResponse} from '@/presentation/protocols';
+import {noContent, serverError, ok} from '@/presentation/helpers';
+import { ListCandidateApplications } from '@/domain/usecases';
+
+export class ListCandidateApplicationsController implements Controller {
+  constructor(
+    private readonly listCandidateApplications: ListCandidateApplications
+  ){}
+
+  async handle(request: ListCandidateApplicationsController.Request): Promise<HttpResponse>{
+    try{
+      const jobVacancies = await this.listCandidateApplications.listCandidateApplications(request);
+
+      return jobVacancies && jobVacancies.length ? ok(jobVacancies) : noContent();
+    }catch(err){
+      return serverError(err as Error);
+    }
+  }
+}
+
+export namespace ListCandidateApplicationsController {
+  export type Request = string;
+}
