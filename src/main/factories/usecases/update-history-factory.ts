88c37f930960a57cfdbc7ce29c80fb88b0784cb7
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-06 16:54:08 -0300

    feat: add UpdateHistoryFactory

---
+++ src/main/factories/usecases/update-history-factory.ts
@@ -0,0 +1,8 @@
+import { DbUpdateHistory } from "@/data";
+import { UpdateHistory } from "@/domain/usecases";
+import { HistorySequelizeRepository } from "@/infra/db/sequelize/repositories";
+
+export const makeDbUpdateHistory = (): UpdateHistory => {
+  const hsitoryRepository = new HistorySequelizeRepository();
+  return new DbUpdateHistory(hsitoryRepository, hsitoryRepository);
+};
