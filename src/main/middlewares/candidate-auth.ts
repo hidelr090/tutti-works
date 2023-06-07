e8af7890b4b463ee1f9193eca5b400701b06f8aa
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-07 16:17:49 -0300

    refactor: simple auth instead especific

--- src/main/middlewares/candidate-auth.ts
+++
@@ -1,5 +0,0 @@
-import { adaptMiddleware } from '@/main/adapters'
-import { makeAuthMiddleware } from '@/main/factories'
-import { UserRole } from '@/utils/enumerators'
-
-export const candidateAuth = adaptMiddleware(makeAuthMiddleware(UserRole.CANDIDATE))
