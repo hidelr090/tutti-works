120335b221e8cf445c78e9d45b7eed6aa48cac4f
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-07 16:18:21 -0300

    feat: add job vacancy routes

---
+++ src/main/routes/job-vacancy-routes.ts
@@ -0,0 +1,18 @@
+import { adaptRoute } from "@/main/adapters";
+import { 
+  makeAddJobVacancyController,
+  makeUpdateJobVacancyController,
+  makeFindJobVacanciesController,
+  makeSubmitApplicationController
+} from '@/main/factories';
+
+import { Router } from 'express';
+import { recruiterAuth } from "@/main/middlewares/recruiter-auth";
+import { auth } from "@/main/middlewares/auth";
+
+export default (router: Router): void => {
+  router.post('/job-vacancy', recruiterAuth, adaptRoute(makeAddJobVacancyController()));
+  router.put('/job-vacancy', recruiterAuth, adaptRoute(makeUpdateJobVacancyController()));
+  router.get('/job-vacancy', adaptRoute(makeFindJobVacanciesController()));
+  router.post('/submit-application', auth, adaptRoute(makeSubmitApplicationController()));
+};
