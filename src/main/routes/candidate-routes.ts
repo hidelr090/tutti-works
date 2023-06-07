e0878eda35bdb6b50d5172cd0d8ffb86c1d3ef0b
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-07 16:18:07 -0300

    feat: add candidate routes

---
+++ src/main/routes/candidate-routes.ts
@@ -0,0 +1,19 @@
+import { adaptRoute } from "@/main/adapters";
+import { 
+  makeAddCandidateController, 
+  makeAddCandidateSocialGroupsController, 
+  makeListCandidateApplicationsController,
+  makeUpdateCandidateController,
+  makeSearchForCandidatesController
+} from '@/main/factories';
+
+import { Router } from 'express';
+import { auth } from "../middlewares/auth";
+
+export default (router: Router): void => {
+  router.post('/candidate', auth, adaptRoute(makeAddCandidateController()));
+  router.post('/candidate-social-groups', auth, adaptRoute(makeAddCandidateSocialGroupsController()));
+  router.get('/candidate-applications', adaptRoute(makeListCandidateApplicationsController()));
+  router.put('/candidate', auth, adaptRoute(makeUpdateCandidateController()));
+  router.get('/candidate-browse', adaptRoute(makeSearchForCandidatesController()));
+};
