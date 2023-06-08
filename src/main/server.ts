2bd45ba02112f2464638d9d06f7c825bc3db1508
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-08 15:03:14 -0300

    feat: add server initalization config

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,6 +3,6 @@ import env from './config/env';
 
 const app = new App();
 
-app.server.listen(env.port, () => 'Running on localhost');
+app.server.listen(Number(env.port), env.hostname ,() => console.log(`Server running on ${env.hostname}/${env.port}`));
 
 export const server = app.server;
