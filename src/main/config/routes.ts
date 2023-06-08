a5d83215d4b4468fa8a42a3233ff3c64eb9e7b43
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-08 15:02:50 -0300

    feat: add router adapter

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,9 +1,13 @@
-import { Router } from 'express';
+import { Express, Router } from 'express'
+import { readdirSync } from 'fs'
+import { join } from 'path'
 
-const router = Router();
-
-router.get('/test', (req, res) => {
-  return res.status(200).send();
-})
-
-export { router };
+export default (app: Express): void => {
+  const router = Router()
+  app.use('/api', router)
+  readdirSync(join(__dirname, '../routes')).map(async file => {
+    if (!file.endsWith('.map')) {
+      (await import(`../routes/${file}`)).default(router)
+    }
+  })
+}
