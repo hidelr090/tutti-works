7e213183838a156c3bbbc6e82ef74fddff5ca4e5
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-08 15:03:45 -0300

    chore: add field to env ts file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   port: process.env.PORT || 3333,
-  jwtSecret: process.env.JWT_SECRET || 'secret'
+  jwtSecret: process.env.JWT_SECRET || 'secret',
+  hostname: process.env.SERVER_HOST || 'localhost'
 }
