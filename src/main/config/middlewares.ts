0301a2b3a13b900c887ab57b4956f356250bc284
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-08 15:04:11 -0300

    refactor: add semicolon to middleware setup

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -3,7 +3,7 @@ import { bodyParser, cors, contentType } from "../middlewares"
 import { Express } from 'express'
 
 export const setupMiddlewares = (app: Express): void => {
-  app.use(bodyParser)
-  app.use(cors)
-  app.use(contentType)
+  app.use(cors);
+  app.use(bodyParser);
+  app.use(contentType);
 }
