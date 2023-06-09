f8786b787130fa61ebcdba75a168626ad5c1ce1d
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-06-09 14:57:45 -0300

    fix: remove issues

--- tests/data/mocks/mock-db-user.ts
+++ tests/data/mocks/mock-db-user.ts
@@ -37,22 +37,17 @@ export class CheckUserByEmailRepositorySpy implements CheckUserByEmailRepository
 }
 
 export class UpdateUserRepositorySpy implements UpdateUserRepository {
-  result: UpdateUserRepository.Result = mockUpdateUserParams();
   data: UpdateUserRepository.Params = mockUpdateUserParams();
   async update (userId: string, data: UpdateUserRepository.Params): Promise<UpdateUserRepository.Result> {
     this.data = data;
-    return {...data, phone: this.result.phone};
   }
 }
 
 export class LoadUserByIdRepositorySpy implements LoadUserByIdRepository {
-  result: UpdateUserRepository.Result = mockUpdateUserParams();
   data: UpdateUserRepository.Params = mockUpdateUserParams();
 
   async loadById (id: string): Promise<LoadUserByIdRepository.Result> {
-    this.result = {...this.data, id };
-
-    return this.result
+    return null;
   }
 }
 
