f0bf1ef7fce26627ed7838a6eba052598a2a6119
Hidelgardo Ramirez <hidelgardo503@gmail.com>
2023-07-22 11:14:58 -0300

    feat: switch api porrt

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  port: process.env.PORT || 3333,
+  port: process.env.PORT || 3334,
   jwtSecret: process.env.JWT_SECRET || 'secret',
   hostname: process.env.SERVER_HOST || 'localhost'
 }
